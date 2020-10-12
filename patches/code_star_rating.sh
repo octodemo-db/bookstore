d42cf5dedfcb728a9e52dbb5410a3864f04abb03
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-10-12 18:09:18 +0000

    Building patch set for star rating feature

--- patches/code_star_rating.sh
+++ patches/code_star_rating.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+feature_pack_tarball=$DIR/star_rating/star_rating.tar.gz
+feature_branch_name="feature-book-star-rating"
 
+current_branch=`git branch`
+echo "Current Branch: $current_branch"
+
+echo "Creating new feature branch: $feature_branch_name"
 git checkout -b feature-book-star-rating
-git apply $DIR/star_rating/star_rating.diff
+
+echo "Applying code changes..."
+pushd $DIR/..
+tar -xvf $feature_pack_tarball
+popd
+
+echo "Completed."
