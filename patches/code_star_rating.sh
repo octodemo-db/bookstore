eee1554fcd730b2e65003c1d23dd4df60eeca372
Peter Murray <681306+peter-murray@users.noreply.github.com>
2020-10-28 20:07:42 +0000

    New patchset

--- patches/code_star_rating.sh
+++ patches/code_star_rating.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-feature_pack_tarball=$DIR/star_rating/star_rating.tar.gz
+feature_pack_tarball=$DIR/star_rating/star_rating.tgz
 feature_branch_name="feature-book-star-rating"
 
 current_branch=`git branch`
